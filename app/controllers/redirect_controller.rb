31ddc0949105ed1418a1a962c0412a3bb2b94e9e
Arun <arun.antony@geckolyst.com>
2023-11-27 12:35:56 +0530

    commit

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,14 +1,11 @@
 class RedirectController < ApplicationController
+
     def index
+      
       shortened_url = params[:shortened_url]
        @user = ShortUrl.find_by(shortened_url: shortened_url)
-       @rec = @user.id
+       @rec = @user.original_url
         redirect_to controller: 'home', action: 'show' 
-      # if short_url_record
-      #   redirect_to short_url_record.original_url
-      # else
-      #   render plain: 'Shortened URL not found'
-      # end
 
     end
   end
