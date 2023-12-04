59be52a7982e8c30a56db956176f979a029d92a1
Arun <arun.antony@geckolyst.com>
2023-12-04 15:51:06 +0530

    commit

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+module.exports = {
+    
+    // Other webpack configurations...
+    node: {
+      __dirname: false,
+      __filename: false,
+      global: true,
+    },
+  
+    // Other webpack configurations...
+  };
+  
